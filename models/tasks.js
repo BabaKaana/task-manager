d4290d9399b604913bc6adc98c3f7a475210c984
BabaKaana <samiulhaq744@gmail.com>
2025-08-30 00:10:00 +0500

    added validations to db entries

--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'name is required!'],
+        maxlength: [25, 'must not be longer than 25 characters'],
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Task', taskSchema);
