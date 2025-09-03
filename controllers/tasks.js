f1616a58c185f02630f28d9a723c91994d98f075
BabaKaana <samiulhaq744@gmail.com>
2025-09-03 23:45:48 +0500

    added async wrapper

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,36 +1,24 @@
 const Task = require('../models/tasks');
+const asyncWrapper = require('../middlewares/async');
 
-const getAllTasks = async (req, res) => {
-  try {
+const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
     res.json({tasks});
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+})
 
-const createTask = async (req, res) => {
-  try {
+const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+})
 
-const getTask = async (req, res) => {
-  try {
+const getTask = asyncWrapper(async (req, res) => {
     const {id: taskId} = req.params;
     const task = await Task.findOne({_id: taskId});
     if (!task) return res.status(404).json({error: `No task with id ${taskId}`});
     res.json({task});
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+})
 
-const updateTask = async (req, res) => {
-  try {
+const updateTask = asyncWrapper(async (req, res) => {
     const {id: taskId} = req.params;
     const task = await Task.findOneAndUpdate({_id: taskId}, req.body, {
       new: true,
@@ -38,21 +26,14 @@ const updateTask = async (req, res) => {
     })
     if (!task) return res.status(404).json({error: `No task with id ${taskId}`});
     res.status(200).json({task});
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+})
 
-const deleteTask = async (req, res) => {
-  try {
+const deleteTask = asyncWrapper( async (req, res) => {
     const {id: taskId} = req.params;
     const task = await Task.findOneAndDelete({_id: taskId});
     if (!task) return res.status(404).json({error: `No task with id ${taskId}`});
     res.status(200).send('Task deleted...');
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+})
 
 
 module.exports = {
